ea6b8629562518eba0dbd31a1f76d5744fcac8a5
Dmitriy <dmitriy@bessalov.ru>
2021-07-25 18:22:37 +0300

    update auth template

--- apps/auth/router.py
+++ apps/auth/router.py
@@ -8,6 +8,7 @@ from .models import users
 import databases
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
+
 templates = Jinja2Templates(directory="templates")
 
 router = APIRouter()
@@ -15,7 +16,6 @@ router = APIRouter()
 database = databases.Database(settings.database_url)
 
 user_db = SQLAlchemyUserDatabase(UserDB, database, users)
-
 
 jwt_authentication = JWTAuthentication(secret=settings.jwt_secret, lifetime_seconds=3600)
 
@@ -34,7 +34,26 @@ router.include_router(
 )
 
 
-@router.get("/")
+@router.get("/login")
 async def get(request: Request):
     return templates.TemplateResponse("auth/login.html", {"request": request})
 
+
+@router.get("/registr")
+async def get(request: Request):
+    return templates.TemplateResponse("auth/registr.html", {"request": request})
+
+
+@router.get("/reset_activation")
+async def get(request: Request):
+    return templates.TemplateResponse("auth/reset_activation.html", {"request": request})
+
+
+@router.get("/reset_password")
+async def get(request: Request):
+    return templates.TemplateResponse("auth/reset_password.html", {"request": request})
+
+
+@router.get("/reset_password_confirm")
+async def get(request: Request):
+    return templates.TemplateResponse("auth/reset_password_confirm.html", {"request": request})
--- main.py
+++ main.py
@@ -26,4 +26,3 @@ if __name__ == "__main__":
         port=settings.server_port,
         reload=True,
     )
-
