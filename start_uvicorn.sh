b87bd2d122f2998d04c9243efd56053ca8832156
Dmitriy <dmitriy@bessalov.ru>
2021-07-30 18:57:30 +0300

    update static

--- start_uvicorn.sh
+++ start_uvicorn.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-# cd /home/www/buddhachess
-# source ./.venv/bin/activate && exec uvicorn main:app --reload
+cd /home/www/buddhachess
+source ./.venv/bin/activate && exec uvicorn main:app --reload
