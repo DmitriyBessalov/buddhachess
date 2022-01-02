098645f84dac507f55df4c86129438d4a756e5dd
dmitriy <dmitriy@bessalov.ru>
2022-01-02 21:08:18 +0300

    -

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!bin/bash
 tmux new-session -d -s 0
-tmux send-keys -t 0 "cd /home/www/buddhachess" C-m
+tmux send-keys -t 0 "cd /var/www/buddhachess" C-m
 sleep 2
 tmux send-keys -t 0 "source ./.venv/bin/activate" C-m
 sleep 8
 tmux send-keys -t 0 "poetry update" C-m
 sleep 15
-tmux send-keys -t 0 "uvicorn app.main:app --reload --workers 4" C-m
+tmux new-session -d -s 0
+tmux send-keys -t 0 "cd app" C-m
+sleep 1
+tmux send-keys -t 0 "uvicorn main:app --reload --workers 4" C-m
